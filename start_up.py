8cce322cd6f211b2bf21d966c2a125a0f8965f8b
azure <1666534783@qq.com>
2023-08-22 15:52:51 +0800

    start-up: print info

--- start_up.py
+++ start_up.py
@@ -5,5 +5,11 @@ env.reset() # 重置环境
 for _ in range(1000):
     env.render() # 显示图形界面
     action = env.action_space.sample() # 随机采样动作
-    env.step(action) # 采取动作
+    observation, reward, done, info = env.step(action) # 采取动作
+    print(observation)
+    print(reward)
+    print(done)
+    print(info)
+    if done is True:
+        break
 env.close() # 关闭环境
